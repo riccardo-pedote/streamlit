faf3f6683e187bc133f9e9d8fd893c2d212d90d4
Riccardo Pedote <85652649+riccardo-pedote@users.noreply.github.com>
2022-11-18 21:09:07 +0100

    Create Wine2Vec.py

---
+++ wine2vec/Wine2Vec.py
@@ -0,0 +1,31 @@
+path='/app/streamlit/'
+import streamlit as st
+import pandas as pd
+from gensim.models import KeyedVectors
+with st.container():
+  st.image(path+'wine_logo.png',use_column_width=True)
+  #emb=Word2Vec.load(path+'wv.vec',binary=True)
+  emb=KeyedVectors.load_word2vec_format(path+'wv.vec',binary=True)
+  tokens=emb.index_to_key
+  tokens=[i.capitalize().replace('_',' ') for i in tokens]
+  pos=st.multiselect('Desired features:',tokens)
+  neg=st.multiselect('Undesired features:',tokens)
+  ''
+  if len(pos)>0 or len(neg)>0:
+    pos=[i.lower().replace(' ','_') for i in pos]
+    neg=[i.lower().replace(' ','_') for i in neg]
+    ms=emb.most_similar(positive=pos,negative=neg,topn=77000)
+    ms=[i[0].capitalize().replace('_',' ') for i in ms]
+    df=pd.read_csv(path+'variety.csv')
+    wines=df.variety.to_list()
+    top=[]
+    for i in ms:
+      if i in wines:
+        top.append(i)
+      if len(top)==5:
+        break
+    for i in top:
+      st.text('• '+i)
+  ''
+  with st.expander('Wine Chart'):
+    st.image(path+'wine_compass.jpg')
