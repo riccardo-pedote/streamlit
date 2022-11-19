e9c14c91d0abff10264a85f7c97a315879022574
Riccardo Pedote <85652649+riccardo-pedote@users.noreply.github.com>
2022-11-19 14:56:49 +0100

    Update Wine2Vec_App.py

--- wine2vec/Wine2Vec_App.py
+++ wine2vec/Wine2Vec_App.py
@@ -8,23 +8,28 @@ with st.container():
   tokens=[i.capitalize().replace('_',' ') for i in emb.index_to_key]
   pos=st.multiselect('Desired features:',tokens)
   neg=st.multiselect('Undesired features:',tokens)
+  df=pd.read_csv(path+'variety.csv')
+  col1,col2=st.columns(2)
+  min_p=col1.slider('Min price:',0,100,0)
+  max_p=col2.slider('Max price:',0,100,100)
   ''
   if len(pos)>0 or len(neg)>0:
     pos=[i.lower().replace(' ','_') for i in pos]
     neg=[i.lower().replace(' ','_') for i in neg]
     ms=emb.most_similar(positive=pos,negative=neg,topn=77000)
     ms=[i[0].capitalize().replace('_',' ') for i in ms]
-    df=pd.read_csv(path+'variety.csv')
     wines=df.variety.to_list()
-    top=[]
+    top,pri=[],[]
     for i in ms:
       if i in wines:
-        top.append(i)
+        p=df.price[df.variety==i].iloc[0]
+        if p>min_p and p<max_p:
+          top.append(i)
+          pri.append(p)
       if len(top)==5:
         break
-    for i in top:
-      p=round(df.price[df.variety==i].iloc[0])
-      st.text(f'• {i} ({p}$)')
+    for i in range(len(top)):
+      st.text(f'• {top[i]} ({round(pri[i])}$)')
   ''
   with st.expander('Wine Chart'):
     st.image(path+'wine_compass.jpg')
